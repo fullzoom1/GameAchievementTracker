5d46a85aa349a5968be22266466ee9c7fd206b9d
fedor <fedor.hlebnikov29@gmail.com>
2026-06-17 14:47:14 +0300

    Update Models.cs

--- PROEKT2/Models.cs
+++ PROEKT2/Models.cs
@@ -7,7 +7,7 @@ namespace PROEKT2
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Platform { get; set; } = string.Empty;
-        public int TimePlayedMinutes { get; set; } // Время в игре
+        public int TimePlayedMinutes { get; set; }
         public List<Achievement> Achievements { get; set; } = new List<Achievement>();
     }
 
@@ -18,6 +18,6 @@ namespace PROEKT2
         public string Description { get; set; } = string.Empty;
         public bool IsUnlocked { get; set; }
         public bool IsTimeBased { get; set; } // Зависит ли от времени
-        public int RequiredMinutes { get; set; } // Сколько минут нужно
+        public int RequiredMinutes { get; set; } 
     }
 }
