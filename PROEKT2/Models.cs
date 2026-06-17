3efaa00e749c4e12a34afeb2664f58d4a133e83b
fedor <fedor.hlebnikov29@gmail.com>
2026-06-17 14:47:30 +0300

    Update Models.cs

--- PROEKT2/Models.cs
+++ PROEKT2/Models.cs
@@ -17,7 +17,7 @@ namespace PROEKT2
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public bool IsUnlocked { get; set; }
-        public bool IsTimeBased { get; set; } // Зависит ли от времени
+        public bool IsTimeBased { get; set; } 
         public int RequiredMinutes { get; set; } 
     }
 }
